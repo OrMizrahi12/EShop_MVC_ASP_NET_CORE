83db822b29bc94b1dfd9c91ecdec2e84c514a4e2
ormizrahi1610 <ormizrahi1610@gmail.com>
2023-04-19 19:05:38 +0300

    after akk

--- EShop.DataAccess/DbInitializer/DbInitializer.cs
+++ EShop.DataAccess/DbInitializer/DbInitializer.cs
@@ -43,6 +43,7 @@ namespace EShop.DataAccess.DbInitializer {
             if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult()) {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
